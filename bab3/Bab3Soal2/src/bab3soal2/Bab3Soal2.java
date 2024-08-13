090cff7a7345f95351d8b111ce87bd8c868684a4
salwanapila <145428116+salwanapila@users.noreply.github.com>
2024-08-13 10:57:32 +0700

    Add files via upload

---
+++ bab3/Bab3Soal2/src/bab3soal2/Bab3Soal2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal2;
+
+/**
+ *
+ * @author user
+ */
+public class Bab3Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int NIM = 2110117000;
+        String namaMahasiswa = "Andika Jaya Utama";
+        String kelas = "01SIFE020";
+        double usia = 19.5;
+        double tinggiBadan = 1.72;
+        
+        System.out.println("NIM             : " + NIM);
+        System.out.println("Nama Mahasiswa  : " + namaMahasiswa);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Usia            : " + usia);
+        System.out.println("Tinggi Badan    : " + kelas);
+    }
+    
+}
