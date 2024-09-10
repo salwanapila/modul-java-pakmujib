889251a7dbaf61d50420c99f9daa94515fa267e6
salwanapila <145428116+salwanapila@users.noreply.github.com>
2024-09-10 10:10:00 +0700

    Add files via upload

---
+++ bab4/Bab4Soal1/src/bab4soal1/Bab4Soal1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soal1;
+
+/**
+ *
+ * @author user
+ */
+public class Bab4Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int angka1 = 10;
+        int angka2 = 3;
+        
+        int hasilTambah = angka1 + angka2;
+        System.out.println("a. Hasil Pertambahan A + B = " + hasilTambah);
+        
+        int hasilKurang = angka1 - angka2;
+        System.out.println("b. Hasil Pengurangan A - B = " + hasilKurang);
+        
+        int hasilKali = angka1 * angka2;
+        System.out.println("c. Hasil Perkalian A * B = " + hasilKali);
+        
+        double hasilBagi = (double) angka1 / angka2;
+        System.out.println("d. Hasil Pembagian A / B = " + hasilBagi);
+    }
+    
+}
---
+++ bab4/Bab4Soal2/src/bab4soal2/Bab4Soal2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soal2;
+
+/**
+ *
+ * @author user
+ */
+public class Bab4Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int w, x, y, z;
+       x = 6;
+       w = 5;
+       y = 8 - x++;
+       z = 8 - ++w;
+       
+       System.out.println("Nilai x = " + x);
+       System.out.println("Nilai w = " + w);
+       System.out.println("Nilai y = " + y);
+       System.out.println("Nilai z = " + z);
+    }
+    
+}
